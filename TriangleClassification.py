1f43539f381772546d424f56d43ef57d0a28de03
smcdonou02 <smcdonou@stevens.edu>
2024-09-22 16:21:47 -0400

    add image of inputs and outputs

--- TriangleClassification.py
+++ TriangleClassification.py
@@ -7,21 +7,19 @@ Author: Stephanie McDonough
 '''
 import math
 
-class TriangleClassification:
-    #
-    def classify_triangle(a,b,c):
-        sides = [a,b,c]
-        h = max(sides)
-        sides.remove(h)
-        if min(sides) < 0:
-            return "Not valid side length"
-        if h == (math.sqrt(a**2 + b**2)):
-            return "Triangle is right triangle"
-        if a == b and b == c:
-            return "Triangle is equilateral"
-        elif a == b or b == c or a == c:
-            return "Triangle is isosceles"
-        elif a != b and b !=c and a != c:
-            return "Triangle is scalene"
+def classify_triangle(a,b,c):
+    sides = [a,b,c]
+    h = max(sides)
+    sides.remove(h)
+    if min(sides) < 0:
+        return "Not valid side length"
+    if h == (math.sqrt(a**2 + b**2)):
+        return "Triangle is right triangle"
+    if a == b and b == c:
+        return "Triangle is equilateral"
+    elif a == b or b == c or a == c:
+        return "Triangle is isosceles"
+    elif a != b and b !=c and a != c:
+        return "Triangle is scalene"
    
 
