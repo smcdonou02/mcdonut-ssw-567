2acecd41a2bf58d797d168381fa7de12b280e0cc
smcdonou02 <smcdonou@stevens.edu>
2024-10-03 13:21:35 -0400

    Update author and clean up code

--- hw_02a/TestTriangle.py
+++ hw_02a/TestTriangle.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Updated Jan 21, 2018
+Updated October 3, 2024
 The primary goal of this file is to demonstrate a simple unittest implementation
 
 @author: jrr
 @author: rk
+@author: sam
 """
 
 import unittest
 
 from Triangle import classifyTriangle
-
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
 class TestTriangles(unittest.TestCase):
     #define multiple sets of tests as functions with names that begin
--- hw_02a/Triangle.py
+++ hw_02a/Triangle.py
@@ -1,31 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
+Updated October 3, 2024
 
 The primary goal of this file is to demonstrate a simple python program to classify triangles
 
 @author: jrr
 @author: rk
+@author: sm
 """
 
 def classifyTriangle(a,b,c):
-    """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
-      BEWARE: there may be a bug or two in this code
-    """
     if a is None or b is None or c is None:
         raise TypeError("Three inputs required")
     
